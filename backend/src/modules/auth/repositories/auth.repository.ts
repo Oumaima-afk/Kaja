f6ea5359c11823b098510e034b4556a7f3cb13da
Oumaima-afk <oumaima_afakkir@hotmail.com>
2026-04-05 20:09:12 +0200

    feat(auth): add prisma module, service and auth repository with the register method

--- backend/src/modules/auth/repositories/auth.repository.ts
+++ backend/src/modules/auth/repositories/auth.repository.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { RegisterDto } from '../dto/register.dto';
+import { User } from 'generated/prisma';
 
 @Injectable()
-export class AuthRepository {}
+export class AuthRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async register(data: RegisterDto): Promise<User> {
+    return await this.prisma.user.create({ data });
+  }
+}
