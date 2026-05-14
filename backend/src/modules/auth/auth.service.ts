9e0c04d6e5653d925eaccb3f560c389cd85280c5
Oumaima-afk <oumaima_afakkir@hotmail.com>
2026-05-14 11:12:50 +0200

    feat:back-auth: clean files

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -9,20 +9,4 @@ export class AuthService {
   register(body: RegisterDto) {
     return this.registerService.execute(body);
   }
-
-  // findAll() {
-  //   return `This action returns all auth`;
-  // }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} auth`;
-  // }
-
-  // update(id: number, updateAuthDto: UpdateAuthDto) {
-  //   return `This action updates a #${id} auth`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} auth`;
-  // }
 }
