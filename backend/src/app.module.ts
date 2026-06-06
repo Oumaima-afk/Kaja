ae7ca11763113782e2775439d44456a3b5863011
Oumaima-afk <oumaima_afakkir@hotmail.com>
2026-06-06 12:50:24 +0200

    feat:back-events: begin implementing the events module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { EventsModule } from './modules/events/events.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, EventsModule],
 })
 export class AppModule {}
